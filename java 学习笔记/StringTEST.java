e9a83de78bea08d2f79236054c22fa384e8d30ef
coder-zt <691076379@qq.com>
2020-07-30 10:51:58 +0800

    7/30

---
+++ java 学习笔记/StringTEST.java
@@ -0,0 +1,10 @@
+
+public class StringTEST {
+
+    public static void main(String[] args) {
+        String str = "this is test and test";
+        str = str.replace("test","check");
+        System.out.println(str);
+    }
+
+}
